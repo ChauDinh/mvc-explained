8bbb7edfe50dcaf69a0fbdddd47f4499c801dcc9
Chau Dinh <chaudvb95uc@gmail.com>
2019-05-05 19:04:15 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# What is MVC?
+
+* What is the M in MVC?
+
+* What is the V in MVC?
+
+* What is the C in MVC?
+
+## Notes
+
+M stands for Model and it represents the data structure you config and save a name representing a entiny in your system.
+
+V stands for View and it represents what you will send to the client making a request to your server.
+
+C stands for Controller and it represents logics (usually wrapped in functions) that will handle the incoming network requests and then return to the client.
+
+The flow usually goes:
+
+client (browser) --> controller --> model --> view --> client (browser)
+
---
+++ src/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.resolve(__dirname, "views"));
+
+/**
+ * This is our controller and this is where we handle incoming network requests
+ * 
+ * The goal of controller is to be the glue that connects the model and the view.
+ * 
+ * It is usually a good idea to group controllers based on functionality but in this
+ * demo, we don't have enough enpoints to warrant that
+ */
+
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views", "static.html"));
+});
+
+app.get("/render", (req, res) => {
+  res.render("render.ejs", { message: "This is the render view" });
+});
+
+app.get("/json", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "views", "still_a_view.json"));
+})
+
+module.exports = app;
